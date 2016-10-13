c833b44bb75556f75f39ce0216bb7b0bc1c6ee24
Maciek Kurek <kurekmaciek@gmail.com>
2016-10-13 22:30:28 +0200

    day 2

---
+++ src/main/webapp/js/controllers/EventController.js
@@ -0,0 +1,43 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope) {
+        $scope.event = {
+            name : 'App Ang',
+            date : '2016-10-12',
+            time : '8 pm',
+            location : {
+                address: "abc",
+                city : "Krk",
+                state : "mpl."
+            },
+            imgUrl : "/img/angularjs-logo.png",
+            sessions : [
+                {
+                    name : "name session A",
+                    creatorName : 'Ante A',
+                    duration : '1 hr',
+                    level : 'advanced',
+                    abstract : 'Lorem ipsum',
+                    upVoteCount : 0
+                },
+                {
+                    name : "session next B",
+                    creatorName : 'Andy B',
+                    duration : '2 hr',
+                    level : '1st level',
+                    abstract : 'Lorem ipsum i pruitum',
+                    upVoteCount : 0
+                },
+                {
+                    name : "another session c",
+                    creatorName : 'Beta Meta',
+                    duration : '5 hr',
+                    level : 'interm',
+                    abstract : 'Lorem ipsum interm interm',
+                    upVoteCount : 0
+                },
+            ]
+        }
+    }
+);
