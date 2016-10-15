93a7c16132ac2f93046b00a45001ce8526a8d2a3
Maciek Kurek <kurekmaciek@gmail.com>
2016-10-15 11:46:26 +0200

    custom filter

--- src/main/webapp/js/controllers/EventController.js
+++ src/main/webapp/js/controllers/EventController.js
@@ -7,46 +7,46 @@ eventsApp.controller('EventController',
 
         $scope.buttonDisabled = true;
 
-        $scope.sortOrder = "['-upVoteCount', 'name']";
+        $scope.sortOrder = "";
         $scope.sortComplex = "['-upVoteCount', 'name']";
         $scope.sortVotesOnly = '-upVoteCount';
         $scope.sortNamesOnly = 'name';
 
         $scope.event = {
-            name : 'Angular_js supported',
-            date : '2016-10-12',
-            time : '8 pm',
-            price : 23,
-            location : {
+            name: 'Angular_js supported',
+            date: '2016-10-12',
+            time: '8 pm',
+            price: 23,
+            location: {
                 address: "abc",
-                city : "Krk",
-                state : "mpl."
+                city: "Krk",
+                state: "mpl."
             },
-            imgUrl : "/img/angularjs-logo.png",
-            sessions : [
+            imgUrl: "/img/angularjs-logo.png",
+            sessions: [
                 {
-                    name : "name session A",
-                    creatorName : 'Ante A',
-                    duration : '1 hr',
-                    level : 'advanced',
-                    abstract : 'Lorem ipsum',
-                    upVoteCount : 0
+                    name: "name session A",
+                    creatorName: 'Ante A',
+                    duration: 1,
+                    level: 'advanced',
+                    abstract: 'Lorem ipsum',
+                    upVoteCount: 0
                 },
                 {
-                    name : "session next B",
-                    creatorName : 'Andy B',
-                    duration : '2 hr',
-                    level : '1st level',
-                    abstract : 'Lorem ipsum i pruitum',
-                    upVoteCount : 0
+                    name: "session next B",
+                    creatorName: 'Andy B',
+                    duration: 2,
+                    level: '1st level',
+                    abstract: 'Lorem ipsum i pruitum',
+                    upVoteCount: 0
                 },
                 {
-                    name : "another session c",
-                    creatorName : 'Beta Meta',
-                    duration : '5 hr',
-                    level : 'intermediate',
-                    abstract : 'Lorem ipsum interm interm',
-                    upVoteCount : 0
+                    name: "another session c",
+                    creatorName: 'Beta Meta',
+                    duration: 4,
+                    level: 'intermediate',
+                    abstract: 'Lorem ipsum interm interm',
+                    upVoteCount: 0
                 },
             ]
         }
@@ -57,5 +57,4 @@ eventsApp.controller('EventController',
         $scope.downVoteSession = function (session) {
             session.upVoteCount--;
         }
-    }
-)
+    })
