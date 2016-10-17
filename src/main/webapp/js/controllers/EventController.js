94922e7d0178693c69e201768e96b9e774fc0b54
Maciek Kurek <kurekmaciek@gmail.com>
2016-10-17 21:26:28 +0200

    custom service

--- src/main/webapp/js/controllers/EventController.js
+++ src/main/webapp/js/controllers/EventController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 eventsApp.controller('EventController',
-    function EventController($scope) {
+    function EventController($scope, eventData) {
 
         $scope.snippet = '<span style="color:red">hi there</span>';
 
@@ -11,44 +11,7 @@ eventsApp.controller('EventController',
         $scope.sortVotesOnly = '-upVoteCount';
         $scope.sortNamesOnly = 'name';
 
-        $scope.event = {
-            name: 'Angular_js supported',
-            date: '2016-10-12',
-            time: '8 pm',
-            price: 23,
-            location: {
-                address: "abc",
-                city: "Krk",
-                state: "mpl."
-            },
-            imgUrl: "/img/angularjs-logo.png",
-            sessions: [
-                {
-                    name: "name session A",
-                    creatorName: 'Ante A',
-                    duration: 1,
-                    level: 'advanced',
-                    abstract: 'Lorem ipsum',
-                    upVoteCount: 0
-                },
-                {
-                    name: "session next B",
-                    creatorName: 'Andy B',
-                    duration: 2,
-                    level: '1st level',
-                    abstract: 'Lorem ipsum i pruitum',
-                    upVoteCount: 0
-                },
-                {
-                    name: "another session c",
-                    creatorName: 'Beta Meta',
-                    duration: 4,
-                    level: 'intermediate',
-                    abstract: 'Lorem ipsum interm interm',
-                    upVoteCount: 0
-                },
-            ]
-        }
+        $scope.event = eventData.event;
 
         $scope.upVoteSession = function (session) {
             session.upVoteCount++;
