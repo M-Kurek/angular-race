c5bb7bd69de08e372956d3b5cdf9871b99db94dc
Maciek Kurek <kurekmaciek@gmail.com>
2016-11-04 22:24:29 +0100

    controller within directive

--- src/main/webapp/js/directives/greeting.js
+++ src/main/webapp/js/directives/greeting.js
@@ -6,11 +6,14 @@ eventsApp.directive('greeting',
             restrict: 'E',
             replace: true,
             template: '<button class="btn" ng-click="sayHello()">Say hello</button>',
-            controller : function ($scope) {
-                $scope.sayHello = function () {
-                    alert("saying hello");
-                }
-            }
-
+            controller: 'GreetController'
         };
+    });
+
+/* U can put it to file in js/controllers as well */
+eventsApp.controller('GreetController',
+    function GreetController($scope) {
+        $scope.sayHello = function () {
+            alert("saying hello now !!");
+        }
     });
