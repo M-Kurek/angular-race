88a4da90aeb100b80c791d9605d6fc3ca5339fcc
Maciek Kurek <kurekmaciek@gmail.com>
2016-11-04 20:42:53 +0100

    directive custom elements

--- src/main/webapp/js/directives.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-eventsApp.directive('mySample',
-    function ($compile) {
-        return {
-            restrict: 'E',
-            template: "<input type='text' ng-model='sampleData' />{{sampleData}}<br/>"
-        };
-    });
