df3cf9574aced6214ee9f4b12d2a90412bb9a66d
Maciek Kurek <kurekmaciek@gmail.com>
2016-11-04 21:08:31 +0100

    directive event handling

---
+++ src/main/webapp/js/directives/datekeys.js
@@ -0,0 +1,40 @@
+'use strict';
+
+eventsApp.directive('dateKeys',
+    function() {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs, controller) {
+                element.on('keydown', function (event) {
+                    if (isNumeric(event.keyCode) || isForwardSlash(event.keyCode) || isNavigationKeycode(event.keyCode)) {
+                        return true;
+                    }
+                    return false;
+                })
+            }
+        };
+        function isForwardSlash(keyCode) {
+            return keyCode === 191;
+        }
+        function isNumeric(keyCode) {
+            console.log("1.key : " + keyCode);
+            return (keyCode > 47 && keyCode < 58) || (keyCode > 95 && keyCode < 106);
+        }
+        function isNavigationKeycode(keyCode) {
+            console.log("2.key : " + keyCode);
+            switch (keyCode) {
+                case 8: //backspace
+                case 35: //end
+                case 36: //home
+                case 37: //left
+                case 38: //up
+                case 39: //right
+                case 40: //down
+                case 45: //ins
+                case 46: //del
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    });
