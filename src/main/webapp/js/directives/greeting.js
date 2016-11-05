cc88b6baae1b5e2fc0e515de624a470e4037c349
Maciek Kurek <kurekmaciek@gmail.com>
2016-11-05 18:32:29 +0100

    directive with shared controller

--- src/main/webapp/js/directives/greeting.js
+++ src/main/webapp/js/directives/greeting.js
@@ -1,19 +1,46 @@
 'use strict';
 
-eventsApp.directive('greeting',
-    function () {
+eventsApp.directive('greeting', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<button class="btn" ng-click="sayHello()">Say hello</button>',
+        controller: function ($scope) {
+            var greetings = ['hello'];
+            $scope.sayHello = function () {
+                alert(greetings.join());
+            }
+            this.addGreeting = function (nextGreet) {
+                greetings.push(nextGreet);
+            }
+        }
+    };
+})
+    .directive('finnish', function () {
         return {
-            restrict: 'E',
-            replace: true,
-            template: '<button class="btn" ng-click="sayHello()">Say hello</button>',
-            controller: 'GreetController'
-        };
-    });
+            restrict: 'A',
+            require: 'greeting',
+            link: function (scope, element, attrs, controller) {
+                controller.addGreeting('hei');
+            }
+        }
+    })
+    .directive('polski', function () {
+        return {
+            restrict: 'A',
+            require: 'greeting',
+            link: function (scope, element, attrs, controller) {
+                controller.addGreeting('czesc');
+            }
+        }
+    })
+;
 
 /* U can put it to file in js/controllers as well */
-eventsApp.controller('GreetController',
-    function GreetController($scope) {
-        $scope.sayHello = function () {
-            alert("saying hello now !!");
-        }
-    });
+/*
+ eventsApp.controller('GreetController',
+ function GreetController($scope) {
+ $scope.sayHello = function () {
+ alert("saying hello now !!");
+ }
+ });*/
